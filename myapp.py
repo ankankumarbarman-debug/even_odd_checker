81170ed2a01521116f523948435b505ff319a83a
ankankumarbarman-debug <ankankumarbarman@gmail.com>
2026-06-14 10:47:45 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,40 +2,45 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def odd_even(num):
-    if num % 2 == 0:
-        return "Number is Even"
+def check_password(password):
+
+    if len(password) < 8:
+        return "🔴 Weak Password"
+
+    has_upper = any(char.isupper() for char in password)
+    has_lower = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+
+    special_chars = "!@#$%^&*()_+-=[]{}|;:,.<>?"
+    has_special = any(char in special_chars for char in password)
+
+    if has_upper and has_lower and has_digit and has_special:
+        return "🟢 Strong Password"
+
+    elif (has_upper or has_lower) and has_digit:
+        return "🟡 Medium Password"
+
     else:
-        return "Number is Odd"
+        return "🔴 Weak Password"
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 @app.route("/check", methods=["POST"])
 def check():
 
-    num = int(request.form["number"])
+    password = request.form["password"]
 
-    if num < 0:
-        result = "Number is Negative"
-    elif num == 0:
-        result = "Number is Zero"
-    else:
-        result = odd_even(num)
+    result = check_password(password)
 
     return f"""
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Result</title>
-    </head>
-    <body style="font-family:Arial;text-align:center;margin-top:100px;">
-        <h1>{result}</h1>
-        <a href="/">Check Another Number</a>
-    </body>
-    </html>
+    <h1>{result}</h1>
+    <a href="/">Check Another Password</a>
     """
+
 
 if __name__ == "__main__":
     app.run(debug=True)
